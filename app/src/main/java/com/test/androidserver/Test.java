f7326dcb3e6bdcc6bc7819d867c2fba70f9ca671
suman das <bloomingsuman@gmail.com>
2019-07-16 23:33:12 +0530

    16.07.19

--- app/src/main/java/com/test/androidserver/Test.java
+++ app/src/main/java/com/test/androidserver/Test.java
@@ -13,6 +13,7 @@ public class Test extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         String ss="Nothing";
+        String kk="lknsaljfn";
         //ettt.setImeActionLabel("Custom text", KeyEvent.KEYCODE_ENTER);
 
     }
